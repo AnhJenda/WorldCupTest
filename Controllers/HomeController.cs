70bab5908ecaa84d64b78e00d9214001215fb0fd
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-11-26 09:59:10 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WorldCup.Models;
+
+namespace WorldCup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Details(int id)
+        {
+            if (id == 1)
+            {
+                return "Viet Nam   10 : 0   England";
+            }
+            else if (id == 2)
+            {
+                return "Argentina   0 : 0   Poturgal";
+            }
+            else if (id == 3)
+            {
+                return "France   1 : 1   Italy";
+            }
+            else if (id == 4)
+            {
+                return "Viet Name   10 : 0   Argentina";
+            }
+            else
+                return "No match found!";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
